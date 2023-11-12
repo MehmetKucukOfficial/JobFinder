48d607b7e5cc0b32a2d4af387070152a8c6dca6c
Mehmet Küçük <23761mehmetkucuk23761@gmail.com>
2023-11-13 01:04:37 +0300

    index and about page have been added.

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express'); // import express
 
 const app = express(); // create express app
 
-app.get('/', (req, res) => {
-  res.status(200).send('INDEX PAGE');
-}); // create route for home page
+//Template Engine
+app.set('view engine', 'ejs'); // set view engine to ejs
 
-const port = 3000; // port to listen on
+//Middleware
+app.use(express.static('public')); // set static folder
+
+//Routes
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 }); // listen on port 3000
